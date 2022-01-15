5041cb72eceb7aa338a9d02908414f6f6dcf6d20
Youhee <youhee.kil@gmail.com>
2022-01-15 11:04:32 +0100

    fixing dropping outliers on  column

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -33,6 +33,7 @@ def go(args):
     min_price = args.min_price
     max_price = args.max_price
     idx = df['price'].between(min_price, max_price)
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
     df = df[idx].copy()
     
     # Convert last_review to datetime
