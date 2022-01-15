b7b63e8669a45cd5046cbde0c2654dcb54a8feba
Youhee <youhee.kil@gmail.com>
2022-01-15 10:57:15 +0100

    changes on main file

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ import hydra
 from omegaconf import DictConfig
 
 _steps = [
-    #"download",
-    #"basic_cleaning",
-    #"data_check",
+    "download",
+    "basic_cleaning",
+    "data_check",
     "data_split",
     "train_random_forest",
     # NOTE: We do not include this in the steps so it is not run by mistake.
     # You first need to promote a model export to "prod" before you can run this,
     # then you need to run this step explicitly
-    #"test_regression_model"
+    "test_regression_model"
 ]
 
 
